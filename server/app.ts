f048ce7a99cf9c76b486ac1cffc9a0a22d5fa29e
Yihua Zhang <yihuazhang@Yihuas-MacBook-Pro.local>
2022-02-15 16:46:42 -0500

    adding CORS

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import * as products from './routes/products';
 
 const PORT = 8000;
 const app = express();
+
+app.use(cors());
 
 // Pretty json output
 app.set('json spaces', 2);
@@ -11,6 +14,6 @@ app.set('json spaces', 2);
 // API
 app.get('/products', products.getProducts);
 
-app.listen(PORT, () => {
+app.listen({ port: PORT }, () => {
   console.log(`server running on port: ${PORT}`);
 });
